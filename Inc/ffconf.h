358fe8ba9dd945a9729b12167cdbb20490b4f220
Zhi Liu <james.zhi.liu@gmail.com>
2017-06-24 22:17:02 -0400

    Attempted to fix sd recovery

--- Inc/ffconf.h
+++ Inc/ffconf.h
@@ -259,7 +259,7 @@
 /      lock feature is independent of re-entrancy. */
 
 #define _FS_REENTRANT    1  /* 0:Disable or 1:Enable */
-#define _FS_TIMEOUT      1000 /* Timeout period in unit of time ticks */
+#define _FS_TIMEOUT      16 /* Timeout period in unit of time ticks */
 #define _SYNC_t          osSemaphoreId 
 /* The _FS_REENTRANT option switches the re-entrancy (thread safe) of the FatFs
 /  module itself. Note that regardless of this option, file access to different
